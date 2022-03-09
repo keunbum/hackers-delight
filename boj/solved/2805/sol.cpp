5d17b9618382d16801db89088e53866b1146656d
keunbum <rkeunbum@gmail.com>
2022-03-09 20:52:21 +0900

    2022.03.09 solving

---
+++ boj/solved/2805/sol.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int low = 0;
+  int high = (int) 1e9;
+  while (low < high) {
+    int mid = low + (high - low + 1) / 2;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += max(a[i] - mid, 0);
+    }
+    if (sum >= (long long) m) {
+      low = mid;
+    } else {
+      high = mid - 1;
+    }
+  }
+  cout << low << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * don't lose your pace. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+*/
