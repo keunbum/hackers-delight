36612887ebf20b38993278f6665ec72a94112887
keunbum <rkeunbum@gmail.com>
2022-06-18 01:37:07 +0900

    2022.06.18

---
+++ boj/solved/2473/sol.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  const int inf = (int) 1e9 + 1;
+  array<int, 3> best{inf, inf, inf};
+  for (int i = 0; i < n; i++) {
+    int L = i + 1;
+    int R = (int) a.size() - 1;
+    while (L < R) {
+      long long S = (long long) a[i] + a[L] + a[R];
+      if (abs(S) < abs(accumulate(best.begin(), best.end(), 0LL))) {
+        best = {a[i], a[L], a[R]};
+        if (S == 0) {
+          break;
+        }
+      }
+      if (S < 0) {
+        L += 1;
+      } else {
+        R -= 1;
+      }
+    }
+  }
+  cout << best[0] << " " << best[1] << " " << best[2] << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * keep calm
+  
+ * stuff you should look for
+  * int overflow, array bounds
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
