52092d68ea2b5313b8da22b1486cf4ea29e5f7e3
keunbum <rkeunbum@gmail.com>
2022-07-26 23:43:33 +0900

    2022.07.26

---
+++ programmers/12981/sol.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+vector<int> solution(int n, vector<string> words) {
+  set<string> s;
+  s.insert(words[0]);
+  for (int i = 1; i < (int) words.size(); i++) {
+    if (words[i - 1].back() != words[i].front() || !s.insert(words[i]).second) {
+      return {i % n + 1, i / n + 1};
+    }
+  }
+  return {0, 0};
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * keep calm
+  
+ * stuff you should look for
+  * int overflow, array bounds
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
