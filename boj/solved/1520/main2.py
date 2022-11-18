b330d56a87754caf2818665492f0d0352356334a
keunbum <rkeunbum@gmail.com>
2022-11-18 19:46:15 +0900

    boj 1520 내리막길  python iterate ver

--- boj/solved/1520/main2.py
+++ boj/solved/1520/main2.py
@@ -7,11 +7,12 @@ def ints(): return map(int, input().split())
 
 h, w = ints()
 a = [list(ints()) for _ in range(h)]
-order = sorted([(i // w, i % w) for i in range(h * w)], key=lambda x: a[x[0]][x[1]], reverse=True)
+order = sorted([i for i in range(h * w)], key=lambda x: a[x // w][x % w], reverse=True)
 dp = [[0] * w for _ in range(h)]
 dp[0][0] = 1
-for i, j in order:
-    for ni, nj in [(i+1,j), (i,j+1), (i-1,j), (i,j-1)]:
+for x in order:
+    i, j = x // w, x % w
+    for ni, nj in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
         if 0 <= ni < h and 0 <= nj < w and a[ni][nj] < a[i][j]:
             dp[ni][nj] += dp[i][j]
 print(dp[h-1][w-1]) 
