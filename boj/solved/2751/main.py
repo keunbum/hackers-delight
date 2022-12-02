ef4f2aaec8e1086380725e3d7813f91665892a7c
keunbum <rkeunbum@gmail.com>
2022-12-02 12:56:39 +0900

    boj 2751 수 정렬하기 (2) * Merge Sort

---
+++ boj/solved/2751/main.py
@@ -0,0 +1,40 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs):print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+def merge(a, st, mid, en):
+    buf = a[st:mid]
+    L = 0
+    R = mid
+    ptr = st
+    while L < (mid - st) and R < en:
+        if buf[L] < a[R]:
+            a[ptr] = buf[L]
+            L += 1
+        else:
+            a[ptr] = a[R]
+            R += 1
+        ptr += 1
+    while L < (mid - st):
+        a[ptr] = buf[L]
+        L += 1
+        ptr += 1
+
+# [st, en)
+def merge_sort(a, st, en):
+    n = en - st
+    if n <= 1:
+        return
+    mid = (st + en) // 2
+    merge_sort(a, st, mid)
+    merge_sort(a, mid, en)
+    merge(a, st, mid, en)
+
+n = int(input())
+a = [int(input()) for _ in range(n)]
+merge_sort(a, 0, n)
+print(*a, sep='\n')
+#print('\n'.join(map(str, a)))
