ba93aa5ca07d8c5101a8caca40dae6dec673ad41
keunbum <rkeunbum@gmail.com>
2022-12-09 01:10:12 +0900

    boj 14470 전자레인지

---
+++ boj/solved/14470/sol.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+  cout << (max(0 - a, 0) * c + (a < 0 ? d : 0) + (b - max(a, 0)) * e) << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH(PERSPECTIVE) (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
