32eb01a94a438bdb3809f4fa3437f5ddaaf1dce8
keunbum <rkeunbum@gmail.com>
2022-12-09 01:16:53 +0900

    boj 12756 고급 여관

---
+++ boj/solved/12756/sol.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  while (b > 0 && d > 0) {
+    b -= c;
+    d -= a;
+  }
+  cout << (b > 0 ? "PLAYER A" : (d > 0 ? "PLAYER B" : "DRAW")) << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH(PERSPECTIVE) (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
