1c858ffefaf8a1f815c97fd1d88d8efec10e9773
keunbum <rkeunbum@gmail.com>
2022-12-10 03:47:02 +0900

    프로그래머스 > 연습문제 > 롤케이크 자르기

---
+++ programmers/132265/sol2.cpp
@@ -0,0 +1,56 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int solution(vector<int> topping) {
+  map<int, int> mpA;
+  map<int, int> mpB;
+  for (int x : topping) {
+    mpB[x] += 1;
+  }
+  int ret = 0;
+  for (int x : topping) {
+    mpA[x] += 1;
+    mpB[x] -= 1;
+    if (mpB[x] == 0) {
+      mpB.erase(x);
+    }
+    ret += mpA.size() == mpB.size();
+  }
+  return ret;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH(PERSPECTIVE) (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
