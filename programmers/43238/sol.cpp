c343b89adce867b2997d0e7516eb17ed53542116
keunbum <rkeunbum@gmail.com>
2022-12-21 02:54:55 +0900

    프로그래머스 > 이분탐색 > 입국심사

---
+++ programmers/43238/sol.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+long long solution(int n, vector<int> times) {
+  long long low = 1;
+  long long high = (long long) 1e18;
+  while (low < high) {
+    long long mid = low + (high - low) / 2;
+    long long cnt = 0;
+    for (int i = 0; i < (int) times.size(); i++) {
+      cnt = min(cnt + mid / times[i], (long long) n);
+    }
+    if (cnt >= n) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH(PERSPECTIVE) (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
