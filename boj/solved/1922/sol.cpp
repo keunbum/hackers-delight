13273daa99fd375187325dab17cdc75469cbd100
keunbum <rkeunbum@gmail.com>
2022-12-21 17:15:21 +0900

    boj 1922 네트워크 연결

---
+++ boj/solved/1922/sol.cpp
@@ -0,0 +1,68 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  vector<tuple<int, int, int>> edges;
+  while (m--) {
+    int x, y, w;
+    cin >> x >> y >> w;
+    --x; --y;
+    if (x != y) {
+      edges.emplace_back(x, y, w);
+    }
+  }
+  sort(edges.begin(), edges.end(), [&](auto& ei, auto& ej) {
+    return get<2>(ei) < get<2>(ej);
+  });
+  vector<int> p(n);
+  iota(p.begin(), p.end(), 0);
+  function<int(int)> Get = [&](int v) {
+    return v == p[v] ? v : p[v] = Get(p[v]);
+  };
+  int ans = 0;
+  for (auto& edge : edges) {
+    auto[x, y, w] = edge;
+    x = Get(x);
+    y = Get(y);
+    if (x != y) {
+      p[x] = y;
+      ans += w;
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH(PERSPECTIVE) (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
