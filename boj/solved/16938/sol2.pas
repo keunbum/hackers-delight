60ea76127a7d134ee7f98f4d86287cfb7a74162b
keunbum <rkeunbum@gmail.com>
2023-01-03 03:49:33 +0900

    boj 16938 캠프 준비 - DFS 풀이

---
+++ boj/solved/16938/sol2.pas
@@ -0,0 +1,26 @@
+{$R-,S-,Q-,I-,O+}
+var
+  n,l,r,x,i,ans: int32;
+  a: array[0..15] of int32;
+
+procedure DFS(v,cnt,sum,min,max: int32);
+begin
+  if v = n + 1 then
+  begin
+    if (cnt >= 2) and (l <= sum) and (sum <= r) and (max-min >= x) then
+      inc(ans);
+    exit;
+  end;
+  DFS(v+1,cnt,sum,min,max);
+  if a[v] < min then min:=a[v];
+  if a[v] > max then max:=a[v];
+  DFS(v+1,cnt+1,sum+a[v],min,max);
+end;
+
+begin
+  read(n,l,r,x);
+  for i:=1 to n do read(a[i]);
+  ans:=0;
+  DFS(1,0,0,round(1e7),0);
+  writeln(ans);  
+end.
