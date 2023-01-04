d2b0cdc73c0070eb5228f40e8060f9f11070d747
keunbum <rkeunbum@gmail.com>
2023-01-05 03:21:02 +0900

    boj 18229 내가 살게, 아냐 내가 살게

---
+++ boj/solved/18229/sol.cpp
@@ -0,0 +1,56 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int h, w, k;
+  cin >> h >> w >> k;
+  vector<vector<int>> a(h, vector<int>(w + 1));
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      cin >> a[i][j + 1];
+    }
+  }
+  for (int j = 0; j < w; j++) {
+    for (int i = 0; i < h; i++) {
+      a[i][j + 1] += a[i][j];
+      if (a[i][j + 1] >= k) {
+        cout << (i + 1) << " " << (j + 1) << '\n';
+        return 0;
+      }
+    }
+  }
+  assert(false);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * deep breathing. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN (Don't be stubborn)
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
