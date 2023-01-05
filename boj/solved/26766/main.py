3136916e38602dd8fcf9293b6959598f9f2825a5
keunbum <rkeunbum@gmail.com>
2023-01-05 23:34:08 +0900

    boj 26766 Serca

---
+++ boj/solved/26766/main.py
@@ -0,0 +1,19 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+s = ''' @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     
+'''
+N, = ints()
+print(s * N)
