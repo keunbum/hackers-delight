5e598239bef0d6c673e6c43ef65d4156bd22e166
keunbum <rkeunbum@gmail.com>
2023-01-10 00:56:45 +0900

    boj 21738 얼음깨기 펭귄

---
+++ boj/solved/21738/sol2.cpp
@@ -0,0 +1,62 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, s, p;
+  cin >> n >> s >> p;
+  --p;
+  vector<vector<int>> g(n);
+  for (int i = 0; i < n - 1; i++) {
+    int x, y;
+    cin >> x >> y;
+    --x; --y;
+    g[x].push_back(y);
+    g[y].push_back(x);
+  }
+  vector<int> dist(n, -1);
+  dist[p] = 0;
+  vector<int> que(1, p);
+  for (int b = 0; b < (int) que.size(); b++) {
+    for (int u : g[que[b]]) {
+      if (dist[u] == -1) {
+        dist[u] = dist[que[b]] + 1;
+        que.push_back(u);
+      }
+    }
+  }
+  sort(dist.begin(), dist.begin() + s);
+  cout << (n - (1 + dist[0] + dist[1])) << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * don't get excited. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN (Don't be stubborn)
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
