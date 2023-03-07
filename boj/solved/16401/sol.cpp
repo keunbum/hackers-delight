3a59917fa123b22bfb0533537611b424ec5c878a
keunbum <rkeunbum@gmail.com>
2023-03-07 21:19:42 +0900

    boj 16401 과자 나눠주기

---
+++ boj/solved/16401/sol.cpp
@@ -0,0 +1,60 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int m, n;
+  cin >> m >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  int low = 0;
+  int high = (int) 1e9;
+  while (low < high) {
+    int mid = (low + high + 1) >> 1;
+    int possible_cnt = 0;
+    for (int i = 0; i < n; ++i) {
+      possible_cnt += a[i] / mid;
+    }
+    if (possible_cnt >= m) {
+      low = mid;
+    } else {
+      high = mid - 1;
+    }
+  }
+  cout << low << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * think rationally. keep calm
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
