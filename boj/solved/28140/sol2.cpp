daa8fc7bccd2f0f0f7a8ea9558fd664db18bc203
keunbum <rkeunbum@gmail.com>
2023-06-01 02:52:03 +0900

    boj 28140 빨강~ 빨강~ 파랑! 파랑! 달콤한 솜사탕!

---
+++ boj/solved/28140/sol2.cpp
@@ -0,0 +1,103 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+const int N = (int) 1e6;
+const int Q = (int) 1e6;
+
+char s[N + 1];
+int ls[Q];
+int rs[Q];
+int perm[Q];
+int res[Q][4];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, q;
+  cin >> n >> q;
+  cin >> s;
+  for (int i = 0; i < q; ++i) {
+    cin >> ls[i] >> rs[i];
+  }
+  iota(perm, perm + q, 0);
+  {
+    sort(perm, perm + q, [&](int i, int j) {
+      return ls[i] < ls[j];
+    });
+    int L = 0;
+    int R = 0;
+    for (int id = 0; id < q; ++id) {
+      int i = perm[id];
+      L = max(L, ls[i]);
+      while (L < n && s[L] != 'R') {
+        L += 1;
+      }
+      R = max(R, L + 1);
+      while (R < n && s[R] != 'R') {
+        R += 1;
+      }
+      res[i][0] = L;
+      res[i][1] = R;
+    }
+  }
+  {
+    sort(perm, perm + q, [&](int i, int j) {
+      return rs[i] > rs[j];
+    });
+    int R = n - 1;
+    int L = n - 1;
+    for (int id = 0; id < q; ++id) {
+      int i = perm[id];
+      R = min(R, rs[i]);
+      while (R >= 0 && s[R] != 'B') {
+        R -= 1;
+      }
+      L = min(L, R - 1);
+      while (L >= 0 && s[L] != 'B') {
+        L -= 1;
+      }
+      res[i][2] = L;
+      res[i][3] = R;
+    }
+  }
+  for (int i = 0; i < q; ++i) {
+    auto& v = res[i];
+    if (v[1] < v[2]) {
+      cout << v[0] << ' ' << v[1] << ' ' << v[2] << ' ' << v[3] << '\n';
+    } else {
+      cout << -1 << '\n';
+    }
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * think rationally. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
---
+++ boj/solved/28140/sol3.cpp
@@ -0,0 +1,71 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.hpp"
+#else
+#define debug(...) void(0)
+#endif
+
+const int N = (int) 1e6;
+
+char s[N + 1];
+int lpos[N + 2];
+int rpos[N + 2];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, q;
+  cin >> n >> q;
+  cin >> s;
+  int ln = 0;
+  int rn = 0;
+  for (int i = 0; i < n; ++i) {
+    if (s[i] == 'R') {
+      lpos[ln++] = i;
+    }
+    if (s[n - 1 - i] == 'B') {
+      rpos[rn++] = ~(n - 1 - i);
+    }
+  }
+  lpos[ln] = lpos[ln + 1] = n + 1;
+  rpos[rn] = rpos[rn + 1] = ~(-1);
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    int i = (int) (lower_bound(lpos, lpos + ln, l) - lpos);
+    int j = (int) (lower_bound(rpos, rpos + rn, ~r) - rpos);
+    if (lpos[i + 1] < ~rpos[j + 1]) {
+      cout << lpos[i] << ' ' << lpos[i + 1] << ' ' << ~rpos[j + 1] << ' ' << ~rpos[j] << '\n';
+    } else {
+      cout << -1 << '\n';
+    }
+  }  
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  * think rationally. keep calm
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
