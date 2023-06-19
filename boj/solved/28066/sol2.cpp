67aa4c2d28a2c1f798feb96dc02ab74c7ff740d2
keunbum <rkeunbum@gmail.com>
2023-06-20 02:08:03 +0900

    boj 28066 타노스는 요세푸스가 밉다

---
+++ boj/solved/28066/sol2.cpp
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, k;
+  cin >> n >> k;
+  deque<int> a(n);
+  iota(a.begin(), a.end(), 1);
+  while ((int) a.size() > k) {
+    a.erase(a.begin() + 1, a.begin() + k);
+    a.push_back(a[0]);
+    a.pop_front();
+  }
+  cout << a[0] << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
