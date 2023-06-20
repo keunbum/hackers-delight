d7ab5e8706755e0ce0aa7c92d0f9ea8ffc5b353a
keunbum <rkeunbum@gmail.com>
2023-06-21 03:03:50 +0900

    boj 28115 등차수열의 합

---
+++ boj/solved/28115/sol.cpp
@@ -0,0 +1,57 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  bool ok = true;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    ok &= (i == 0 || (a[i] - a[i - 1] == a[1] - a[0]));
+  }
+  if (!ok) {
+    cout << "NO" << '\n';
+    return 0;
+  }
+  cout << "YES" << '\n';
+  for (int i = 0; i < n; ++i) {
+    cout << 0 << " \n"[i == n - 1];
+  }
+  for (int i = 0; i < n; ++i) {
+    cout << a[i] << " \n"[i == n - 1];
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
