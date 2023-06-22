0307ab8b393a14b9f82927d9972f8a1e36257ede
keunbum <rkeunbum@gmail.com>
2023-06-22 22:08:32 +0900

    boj 28245 배고파(Hard)

---
+++ boj/solved/28245/sol.cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  using ull = unsigned long long;
+  vector<ull> a;
+  vector<pair<int, int>> b;
+  for (int y = 0; y < 63; ++y) {
+    for (int x = 0; x <= y; ++x) {
+      a.push_back((1ULL << y) + (1ULL << x));
+      b.emplace_back(x, y);
+    }
+  }
+  int n;
+  cin >> n;
+  while (n--) {
+    ull m;
+    cin >> m;
+    int i = (int) (upper_bound(a.begin(), a.end(), m) - a.begin()) - 1;
+    if (i == -1 || a[i + 1] - m < m - a[i]) {
+      i += 1;
+    }
+    cout << b[i].first << ' ' << b[i].second << '\n';
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
---
+++ boj/solved/28245/sol2.cpp
@@ -0,0 +1,60 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  while (n--) {
+    long long m;
+    cin >> m;
+    int i = -1, j = -1;
+    for (int p = 63; p >= 0; --p) {
+      if ((m >> p) & 1) {
+        if (i == -1) i = p; else
+        if (j == -1) j = p;
+      }
+    }
+    assert(i != -1);
+    if (j == -1) {
+      i -= (i > 0);
+      cout << i << ' ' << i << '\n';
+    } else {
+      if (((1LL << i) + (1LL << (j + 1))) - m < m - ((1LL << i) + (1LL << j)))
+        j += 1;
+      cout << j << ' ' << i << '\n';
+    }
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
