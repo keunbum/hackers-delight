6fde66ec20598ef114dd53d1736a39a1bf324a58
keunbum <rkeunbum@gmail.com>
2023-06-22 22:07:03 +0900

    boj 28239 배고파(Easy)

---
+++ boj/solved/28239/sol.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  while (n--) {
+    long long m;
+    cin >> m;
+    debug(m);
+    int i = -1, j = -1;
+    for (int p = 63; p >= 0; --p) {
+      if ((m >> p) & 1) {
+        if (i == -1) i = p; else
+        if (j == -1) j = p;
+      }
+    }
+    if (j != -1) cout << j << ' ' << i << '\n';
+    else {
+      if (i == 0) i = 1;
+      cout << (i - 1) << ' ' << (i - 1) << '\n';
+    }
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
