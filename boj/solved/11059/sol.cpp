cb084f983ac50e3f0a824d3daa55e715dd9be3ee
keunbum <rkeunbum@gmail.com>
2023-06-27 19:05:37 +0900

    boj 11059 크리 문자열

---
+++ boj/solved/11059/sol.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  int n = (int) s.size();
+  vector<int> pref(n + 1);
+  pref[0] = 0;
+  for (int i = 0; i < n; ++i) {
+    pref[i + 1] = pref[i] + (int) (s[i] - '0');
+  }
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; j += 2) {
+      int k = (i + j) / 2;
+      if (pref[k + 1] - pref[i] == pref[j + 1] - pref[k + 1]) {
+        ans = max(ans, j - i + 1);
+      }
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
