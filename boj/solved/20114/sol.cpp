572cc7573f873168d045467316ada42becfecfd8
keunbum <rkeunbum@gmail.com>
2023-06-27 19:06:28 +0900

    boj 20114 미아 노트

---
+++ boj/solved/20114/sol.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, h, w;
+  cin >> n >> h >> w;
+  vector<string> s(h);
+  for (int i = 0; i < h; ++i) {
+    cin >> s[i];
+  }
+  string ans(n, '?');
+  for (int j = 0; j < w * n; j++) {
+    char ch = '?';
+    for (int i = 0; i < h; ++i) {
+      if (s[i][j] != '?') {
+        ch = s[i][j];
+      }
+    }
+    if (ch != '?') {
+      ans[j / w] = ch;
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
