5fbf7ca9a77c3aa423f69d4ae1b7cb18badcf79f
keunbum <rkeunbum@gmail.com>
2023-06-27 19:08:20 +0900

    boj 3167 기차표 검사

---
+++ boj/solved/3167/sol.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, k;
+  cin >> n >> k;
+  vector<int> in(n), out(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> out[i] >> in[i];
+  }
+  {
+    int A = 0, B = 0;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int v = min(A, out[i]);
+      A -= v;
+      ans += out[i] - v;
+      B -= out[i] - v;
+      B += in[i];
+      if (i % k == 0) {
+        A += B;
+        B = 0;
+      }
+    }
+    cout << ans << ' ';
+  }
+  {
+    int A = 0, B = 0;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int v = min(B, out[i]);
+      B -= v;
+      ans += v;
+      A -= out[i] - v;
+      B += in[i];
+      if (i % k == 0) {
+        A += B;
+        B = 0;
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
+}
