cdbaf2b4fe7d6c8cf9e7d7fc5ae6474d1f12791b
keunbum <rkeunbum@gmail.com>
2023-06-27 19:06:09 +0900

    boj 2405 세 수, 두 M

---
+++ boj/solved/2405/sol.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  int ans = 0;
+  for (int i = 0; i < n - 2; ++i) {
+    ans = max(ans, a[n - 1] - a[i + 1] - (a[i + 1] - a[i]));
+  }
+  for (int i = n - 1; i > 1; --i) {
+    ans = max(ans, a[i - 1] - a[0] - (a[i] - a[i - 1]));
+  }
+  cout << ans << '\n';
+  return 0;
+}
