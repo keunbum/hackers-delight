4bf3075ff2a91c8f445ff6f1f7b9d744ad232182
keunbum <rkeunbum@gmail.com>
2023-06-29 00:27:49 +0900

    boj 8320 직사각형을 만드는 방법

---
+++ boj/solved/8320/sol.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  int ans = 0;
+  for (int x = 1; x <= n; ++x) {
+    for (int i = 1; i * i <= x; ++i) {
+      ans += (x % i == 0);
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
