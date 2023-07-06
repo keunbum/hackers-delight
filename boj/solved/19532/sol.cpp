f715c83a367a338cc158071edadddaf361a55a61
keunbum <rkeunbum@gmail.com>
2023-07-07 01:22:51 +0900

    boj 19532 수학은 비대면강의입니다

---
+++ boj/solved/19532/sol.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "template/debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b, c, d, e, f;
+  cin >> a >> b >> c >> d >> e >> f;
+  for (int x = -999; x <= 999; ++x) for (int y = -999; y <= 999; ++y)
+    if (a * x + b * y == c && d * x + e * y == f) cout << x << ' ' << y << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
