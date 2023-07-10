8c5be464b83528870c5bc7fd587bb124b24a1997
keunbum <rkeunbum@gmail.com>
2023-07-10 20:38:23 +0900

    boj 2338 긴자리 계산

---
+++ boj/solved/2338/main.py
@@ -0,0 +1,11 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+a = int(input())
+b = int(input())
+print(a + b, a - b, a * b, sep='\n')
+
