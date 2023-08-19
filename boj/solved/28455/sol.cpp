a2859b17b351c840f535493fae00590b049800e7
keunbum <rkeunbum@gmail.com>
2023-08-20 03:19:37 +0900

    boj 28455 Union Maplestory

---
+++ boj/solved/28455/sol.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) cin >> a[i];
+  sort(a.rbegin(), a.rend());
+  int lsum = 0;
+  int asum = 0;
+  for (int i = 0; i < min(n, 42); ++i) {
+    lsum += a[i];
+    for (int y : {60, 100, 140, 200, 250}) {
+      asum += a[i] >= y;
+    }
+  }
+  cout << lsum << ' ' << asum << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
