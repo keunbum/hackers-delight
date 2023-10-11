1083a7c7f1e8931af98dc1d924f545fcf0641367
keunbum <rkeunbum@gmail.com>
2023-10-12 03:23:30 +0900

    프로그래머스 > 연습문제 > 달리기 경주

---
+++ programmers/178871/sol.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+vector<string> solution(vector<string> a, vector<string> b) {
+  map<string, int> mp;
+  int n = (int) a.size();
+  for (int i = 0; i < n; ++i) {
+    mp[a[i]] = i;
+  }
+  for (auto& s : b) {
+    int i = mp[s];
+    swap(a[i], a[i - 1]);
+    mp[a[i]] = i;
+    mp[a[i - 1]] = i - 1;
+  }
+  return a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
