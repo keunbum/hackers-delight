3dd183582a780563b8ec15f5577c88db01411519
keunbum <rkeunbum@gmail.com>
2023-10-18 17:53:45 +0900

    프로그래머스 > 연습문제 > 바탕화면 정리

---
+++ programmers/161990/sol.cpp
@@ -0,0 +1,56 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+vector<int> solution(vector<string> s) {
+  int h = (int) s.size();
+  int w = (int) s[0].size();
+  int minx = h, miny = w;
+  int maxx = -1, maxy = -1;
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      if (s[i][j] == '#') {
+        minx = min(minx, i);
+        miny = min(miny, j);
+        maxx = max(maxx, i);
+        maxy = max(maxy, j);
+      }
+    }
+  }
+  return {minx, miny, maxx + 1, maxy + 1};
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
