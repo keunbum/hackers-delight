506d70814a0af29c40e3ceba9968951aa60dfee7
keunbum <rkeunbum@gmail.com>
2023-11-09 00:52:01 +0900

    boj 9517 VOLIM

---
+++ boj/solved/9517/sol.cpp
@@ -0,0 +1,57 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int who;
+  cin >> who;
+  --who;
+  int tt;
+  cin >> tt;
+  int t = 0;
+  while (tt--) {
+    int x;
+    char y;
+    cin >> x >> y;
+    t += x;
+    if (t >= 210) {
+      cout << (who + 1) << '\n';
+      break;
+    }
+    if (y == 'T') {
+      who = (who + 1) & 7;
+    }
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
