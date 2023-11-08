9cf60e2f2e27ad77e0b183521968e16fd862348f
keunbum <rkeunbum@gmail.com>
2023-11-08 20:31:07 +0900

    boj 5341 Pyramids

---
+++ boj/solved/5341/sol.py
@@ -0,0 +1,12 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(n * (n + 1) // 2)
