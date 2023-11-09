b6e36d0c509665f022c94ee015fbf5ee81f7d187
keunbum <rkeunbum@gmail.com>
2023-11-09 23:51:33 +0900

    boj 15784 질투진서

---
+++ boj/solved/15784/sol.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, x, y;
+  cin >> n >> x >> y;
+  --x; --y;
+  vector<vector<int>> a(n, vector<int>(n));
+  for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> a[i][j];
+  bool found = false;
+  for (int i = 0; i < n; ++i) found |= a[i][y] > a[x][y];
+  for (int j = 0; j < n; ++j) found |= a[x][j] > a[x][y];
+  cout << (found ? "ANGRY" : "HAPPY") << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
