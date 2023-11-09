bc4c788aea4c310e682d3e288ca2d04472f99753
keunbum <rkeunbum@gmail.com>
2023-11-10 00:41:25 +0900

    boj 14761 FizzBuzz

---
+++ boj/solved/14761/sol.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int x, y, n;
+  cin >> x >> y >> n;
+  for (int i = 1; i <= n; ++i) {
+    if (i % x == 0 && i % y == 0) cout << "FizzBuzz"; else
+    if (i % x == 0) cout << "Fizz"; else
+    if (i % y == 0) cout << "Buzz";
+    else cout << i;
+    cout << '\n';
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
