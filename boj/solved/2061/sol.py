a782156970f3b32a118b550590b47b9db1b95693
keunbum <rkeunbum@gmail.com>
2023-11-09 23:39:05 +0900

    boj 2061 The Embarrassed Cryptographer

---
+++ boj/solved/2061/sol.py
@@ -0,0 +1,19 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+K, L = ints()
+i = 2
+ok = True
+while i * i <= K and i < L:
+    if K % i == 0:
+        ok = False
+        break
+    i += 1
+if ok:
+    print('GOOD')
+else:
+    print('BAD', i)
