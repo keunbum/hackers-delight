1778f8d4e31929c0a6343cb7f3552cd94f6dcdc1
keunbum <rkeunbum@gmail.com>
2023-11-10 18:19:44 +0900

    boj 27983 리본 (Easy)

---
+++ boj/solved/27983/sol.cpp
@@ -0,0 +1,56 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> X(n);
+  vector<int> L(n);
+  vector<char> C(n);
+  for (int i = 0; i < n; ++i) cin >> X[i];
+  for (int i = 0; i < n; ++i) cin >> L[i];
+  for (int i = 0; i < n; ++i) cin >> C[i];
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      if (abs(X[i] - X[j]) <= L[i] + L[j] && C[i] != C[j]) {
+        cout << "YES" << '\n';
+        cout << (i + 1) << " " << (j + 1) << '\n';
+        return 0;
+      }
+    }
+  }
+  cout << "NO" << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
