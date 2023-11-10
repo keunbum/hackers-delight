0660da04233ebec511c39a4decd9f594901d02b8
keunbum <rkeunbum@gmail.com>
2023-11-10 23:21:29 +0900

    boj 10834 벨트

---
+++ boj/solved/10834/sol.cpp
@@ -0,0 +1,49 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int cs = 1;
+  int bit = 0;
+  int m;
+  cin >> m;
+  while (m--) {
+    int a, b, s;
+    cin >> a >> b >> s;
+    cs = cs / a * b;
+    bit ^= s;
+  }
+  cout << bit << " " << cs << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
