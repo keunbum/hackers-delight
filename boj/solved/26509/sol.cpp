0168ff4fb1c97101dc185667968dcb0fd1d6f2d8
keunbum <rkeunbum@gmail.com>
2023-11-10 19:01:19 +0900

    boj 26509 Triangle

---
+++ boj/solved/26509/sol.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    bool ok = true;
+    set<int> ss[2];
+    for (int i = 0; i < 2; ++i) {
+      int x, y, z;
+      cin >> x >> y >> z;
+      if (x > y) swap(x, y);
+      if (y > z) swap(y, z);
+      ok &= x * x + y * y == z * z;
+      ss[i].insert({x, y , z});
+    }
+    cout << (ok && ss[0] == ss[1] ? "YES" : "NO") << '\n';
+  }
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
