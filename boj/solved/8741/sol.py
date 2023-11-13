fb47568b380cbef5837319a31544c0839cad7c54
keunbum <rkeunbum@gmail.com>
2023-11-13 23:07:24 +0900

    boj 8741 Suma

---
+++ boj/solved/8741/sol.py
@@ -0,0 +1,10 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+k = int(input())
+n = 1 << k
+print(bin(n * (n - 1) // 2)[2:])
