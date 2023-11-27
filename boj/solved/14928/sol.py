8d5868254f6eb80f37e14753cacd5ac4f6b928f4
keunbum <rkeunbum@gmail.com>
2023-11-28 00:52:48 +0900

    boj 14928 큰 수 (BIG)

---
+++ boj/solved/14928/sol.py
@@ -0,0 +1,8 @@
+﻿import sys
+
+input = sys.stdin.readline
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
+def strs(): return input().split()
+def ints(): return map(int, input().split())
+
+print(int(input()) % 20000303)
