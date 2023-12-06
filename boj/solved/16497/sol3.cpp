bbfbf11421bbd2641f7c30f38f5d4e289a13c24c
keunbum <rkeunbum@gmail.com>
2023-12-07 01:59:23 +0900

    boj 16497 대출 요청

---
+++ boj/solved/16497/sol3.cpp
@@ -0,0 +1,57 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) void(0)
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<array<int, 2>> a(n + n);
+  for (int i = 0; i < n; ++i) {
+    int x, y;
+    cin >> x >> y;
+    a[i + i] = {x, +1};
+    a[i + i + 1] = {y, -1};
+  }
+  sort(a.begin(), a.end());
+  int k;
+  cin >> k;
+  for (auto[x, t] : a) {
+    if ((k -= t) < 0) {
+      cout << 0 << '\n';
+      return 0;
+    }
+  }
+  cout << 1 << '\n';
+  return 0;
+}
+
+/* read it as if you were wrong once. --> "why is this wrong??"
+
+ * basic strategy:
+  * don't be obsessed with speed or memory when the input is small compared to limit
+  * internalization of problem statements
+  * simplify. a step-by-step approach
+  * readability is important
+  
+ * stuff you should look for
+  * 0-based or 1-based?
+  * off-by-one error
+  * int overflow, array bounds (habituation of assert and debug)
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH (feat. BFS)
+  
+ * after solving the problem
+  * consider whether there is another way.
+  * reduce memory, time, codes, ...
+  * what is my weakness that need to be addressed by solving this problem?
+*/
