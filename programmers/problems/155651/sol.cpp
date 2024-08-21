a71b98a199cf4b81b026e4abd441052784c4c4d8
keunbum <rkeunbum@gmail.com>
2024-08-22 00:17:36 +0900

    프로그래머스 > 연습문제 > 호텔 대실

---
+++ programmers/problems/155651/sol.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <cstdio>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+int Encode(string& s) {
+    int h, m;
+    sscanf(s.data(), "%d:%d", &h, &m);
+    return 60 * h + m;
+}
+
+int solution(vector<vector<string>> book_time) {
+    int n = int(book_time.size());
+    vector<array<int, 2>> a(n + n);
+    for (int i = 0; i < n; ++i) {
+        a[i + i] = {Encode(book_time[i][0]), +1};
+        a[i + i + 1] = {Encode(book_time[i][1]) + 10, -1};
+    }
+    sort(a.begin(), a.end());
+    int bal = 0;
+    int ret = 0;
+    for (auto& [_, type] : a) {
+        bal += type;
+        ret = max(ret, bal);
+    }
+    return ret;
+}
---
+++ programmers/problems/155651/sol2.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+#include <cstdio>
+#include <array>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int Encode(string& s) {
+    int h, m;
+    sscanf(s.data(), "%d:%d", &h, &m);
+    return 60 * h + m;
+}
+
+int solution(vector<vector<string>> book_time) {
+    int n = int(book_time.size());
+    vector<array<int, 2>> a(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            a[i][j] = Encode(book_time[i][j]) + j * 10;
+        }
+    }
+    sort(a.begin(), a.end());
+    priority_queue<int> s;
+    int ret = 0;
+    for (auto& [beg, end] : a) {
+        while (!s.empty() && ~s.top() <= beg) {
+            s.pop();
+        }
+        s.push(~end);
+        ret = max(ret, int(s.size()));
+    }
+    return ret;
+}
